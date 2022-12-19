11a84901e3f2191d64bde51f96ece5e9dcabb3f9
Shahid Ameen <iamshahidameen@gmail.com>
2022-12-20 02:47:09 +0500

    created FormRow.js component and make field attributes dynamic

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Logo } from '../components';
+import { Logo, FormRow } from '../components';
 import Wrapper from '../assets/wrappers/RegisterPage';
 
 const initialState = {
@@ -12,7 +12,7 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
   const handleChange = (e) => {
-    console.log(e.target.value);
+    console.log(e.target);
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,20 +24,27 @@ const Register = () => {
         <Logo />
         <h3>Login</h3>
 
-        {/* name field */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-
-          <input
-            type="text"
-            value={values.name}
-            name="name"
-            onChange={handleChange}
-            className="form-input"
-          />
-        </div>
+        {/* Name field */}
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* Email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* Password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
 
         <button type="submit" className="btn btn-block">
           submit
