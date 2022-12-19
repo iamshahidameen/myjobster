8b8edd7a45587af9574fb609fa7182d4838b29f3
Shahid Ameen <iamshahidameen@gmail.com>
2022-12-20 01:03:54 +0500

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Landing, Error, Dashboard, Register } from './pages/';
-import styled from 'styled-components';
 function App() {
   return (
     <BrowserRouter>
