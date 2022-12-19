4a142c6a6f1d75529b93492b98ff5cfa4a102d1b
Shahid Ameen <iamshahidameen@gmail.com>
2022-12-19 22:47:42 +0500

    Added Landing page markup and added styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Landing from './pages/Landing';
+import styled from 'styled-components';
 
+const Button = styled.button`
+  background-color: red;
+  color: white;
+  font-size: 2rem;
+`;
+const SecondButton = styled.button`
+  background-color: Blue;
+  color: white;
+  font-size: 3rem;
+`;
 function App() {
   return (
     <div>
+      <Button>Click Me</Button>
+      <SecondButton>Click Me</SecondButton>
       <Landing />
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,28 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I am baby anim chicharrones minim deep v letterpress try-hard, ennui
+            XOXO qui chartreuse incididunt vibecession. Butcher whatever
+            cardigan incididunt. Elit tonx lo-fi fanny pack tacos pok pok
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
