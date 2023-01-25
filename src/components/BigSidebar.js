d39bd97ae991e9e877cdecad451e02473c305b48
Shahid Ameen <iamshahidameen@gmail.com>
2023-01-26 04:39:47 +0500

    Big Sidebar toggle functionality and Links showing in sidebar

--- src/components/BigSidebar.js
+++ src/components/BigSidebar.js
@@ -1,16 +1,23 @@
 import Wrapper from '../assets/wrappers/BigSidebar';
 import NavLinks from './NavLinks';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import Logo from './Logo';
 
 const BigSidebar = ({ toggleSidebar }) => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   const dispatch = useDispatch();
   const toggle = () => {
     dispatch(toggleSidebar());
   };
   return (
     <Wrapper>
-      BigSidebar
-      <NavLinks toggleSidebar={toggle} />
+      <div className={`sidebar-container ${isSidebarOpen && 'show-sidebar'}`}>
+        <div className="content"> </div>
+        <header>
+          <Logo />
+        </header>
+        <NavLinks />
+      </div>
     </Wrapper>
   );
 };
