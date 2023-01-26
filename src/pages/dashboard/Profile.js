eb52a38764e6988ae791565b2f67faba3cdf8e8d
Shahid Ameen <iamshahidameen@gmail.com>
2023-01-26 23:51:04 +0500

    Added dynamic forms /controlled input for Profile

--- src/pages/dashboard/Profile.js
+++ src/pages/dashboard/Profile.js
@@ -8,18 +8,63 @@ import { updateUser } from '../../features/user/userSlice';
 const Profile = () => {
   const { isLoading, user } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    name: user?.name || '',
+    email: user?.email || '',
+    location: user?.location || '',
+    lastName: user?.lastName || '',
+  });
+
+  const handleSubmit = (e) => {
+    const { name, email, location, lastName } = userData;
+    e.preventDefault();
+    if (!name || !email || !location || !lastName) {
+      toast.error('Please fill all fields');
+      return;
+    }
+  };
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setUserData({ ...userData, [name]: value });
+  };
+
+  console.log(userData);
 
   return (
     <Wrapper>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h3>profile</h3>
         <div className="form-center">
-          <FormRow type="text" name="name" />
-          <FormRow type="text" labelText="last name" name="lastName" />
-          <FormRow type="email" name="email" />
-          <FormRow type="text" name="location" />
+          <FormRow
+            type="text"
+            name="name"
+            value={userData.name}
+            handleChange={(e) => handleChange(e)}
+          />
+          <FormRow
+            type="text"
+            labelText="last name"
+            name="lastName"
+            value={userData.lastName}
+            handleChange={(e) => handleChange(e)}
+          />
+          <FormRow
+            type="email"
+            name="email"
+            value={userData.email}
+            handleChange={(e) => handleChange(e)}
+          />
+          <FormRow
+            type="text"
+            name="location"
+            value={userData.location}
+            handleChange={(e) => handleChange(e)}
+          />
           <button type="submit" className="btn btn-block" disabled={isLoading}>
-            save changes
+            {isLoading ? 'Please wait...' : 'save changes'}
           </button>
         </div>
       </form>
