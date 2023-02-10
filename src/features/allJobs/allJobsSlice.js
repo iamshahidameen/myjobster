237b303dc184a6c892cdee0687d6ebbb38ce21ee
Shahid Ameen <iamshahidameen@gmail.com>
2023-02-10 22:57:59 +0500

    query parameters added in the request

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -20,10 +20,16 @@ const initialState = {
   monthlyApplications: [],
   ...initialFilterState,
 };
+
 export const getAllJobs = createAsyncThunk(
   'allJobs/getJobs',
   async (_, thunkAPI) => {
-    let url = `/jobs`;
+    const { page, search, searchStatus, searchType, sort } =
+      thunkAPI.getState().allJobs;
+    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}&page=${page}`;
+    if (search) {
+      url = url + `&search=${search}`;
+    }
 
     try {
       const resp = await customFetch.get(url);
